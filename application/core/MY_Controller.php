c58fe0d2b9f45c2920b065292cbd01a27186abea
webberpuma <webberpuma@gmail.com>
2017-04-11 11:33:10 +0800

    cms tutorial step 3.1: create the login and logout page with ion auth

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -39,6 +39,14 @@ class Admin_Controller extends MY_Controller
   function __construct()
   {
     parent::__construct();
+
+    $this->load->library('ion_auth');
+    if (!$this->ion_auth->logged_in())
+    {
+      //redirect them to the login page
+      redirect('admin/user/login', 'refresh');
+    }
+
     $this->data['page_title'] = 'CI App - Dashboard';
   }
 

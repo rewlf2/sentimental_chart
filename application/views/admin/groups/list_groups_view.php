508468b67174e7ad684373d57f11505606cfbc36
webberpuma <webberpuma@gmail.com>
2017-04-11 12:15:43 +0800

    cms tutorial step 3.3: administer users with ion auth

--- application/views/admin/groups/list_groups_view.php
+++ application/views/admin/groups/list_groups_view.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-<div class="container" style="margin-top: 40px;">
+<div class="container" style="margin-top: 60px;">
   <div class="row">
     <div class="col-lg-12">
       <a href="<?php echo site_url('admin/groups/create');?>" class="btn btn-primary">Create group</a>
@@ -11,10 +11,11 @@
       if(!empty($groups))
       {
         echo '<table class="table table-hover table-bordered table-condensed">';
+        echo '<tr><td>ID</td><td>Group name</td></td><td>Group description</td><td>Operations</td></tr>';
         foreach($groups as $group)
         {
           echo '<tr>';
-          echo '<td>'.$group->name.'</td><td>'.$group->description.'</td><td>'.anchor('admin/groups/edit/'.$group->id,'<span class="glyphicon glyphicon-pencil"></span>');
+          echo '<td>'.$group->id.'</td><td>'.anchor('admin/users/index/'.$group->id,$group->name).'</td><td>'.$group->description.'</td><td>'.anchor('admin/groups/edit/'.$group->id,'<span class="glyphicon glyphicon-pencil"></span>');
           if(!in_array($group->name, array('admin','members'))) echo ' '.anchor('admin/groups/delete/'.$group->id,'<span class="glyphicon glyphicon-remove"></span>');
           echo '</td>';
           echo '</tr>';

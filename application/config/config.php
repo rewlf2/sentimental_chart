baa55cc7902eef43114053a59301aa2e4c3f7601
webberpuma <webberpuma@gmail.com>
2017-04-25 12:26:30 +0800

    change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://webtest:8888/ci3test';
+$config['base_url'] = 'http://webtest/ci3test';
 
 /*
 |--------------------------------------------------------------------------

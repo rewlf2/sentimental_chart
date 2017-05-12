85d6c3db82bb6c59e75174d98c0a6fce1d45e033
webberpuma <webberpuma@gmail.com>
2017-05-12 11:03:40 +0800

    modify config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci3base';
+$config['base_url'] = 'http://beacon';
 
 /*
 |--------------------------------------------------------------------------

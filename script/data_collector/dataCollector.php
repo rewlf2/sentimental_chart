06dc6ac0c9196878331008609a21e348c6ea3579
Pokyan <pokyan@daylightpartnership.com>
2017-05-26 18:05:47 +0800

    Add script starting ending message

--- script/data_collector/dataCollector.php
+++ script/data_collector/dataCollector.php
@@ -12,8 +12,12 @@ $mysqli->set_charset(MysqlConfig::CHARSET);
 $mysql_driver = new mysqli_driver;
 $mysql_driver->report_mode = MYSQLI_REPORT_STRICT | MYSQLI_REPORT_ERROR;
 
+echo "Starting calling api at ", (new Datetime(null, new DatetimeZone("utc")))->format("Y-m-d H:i:s"), "\n";
+
 foreach($ids as $id){
     $hexagon = new CrimsonHexagonApiFetching($id, $mysqli);
     $hexagon->fetchSentiment();
     $hexagon->fetchVolume();
 }
+
+echo "finishing at ", (new Datetime(null, new DatetimeZone("utc")))->format("Y-m-d H:i:s"), "\n";
